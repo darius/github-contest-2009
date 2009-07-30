d873343779fcecaf2f23a8613d0a7bafbe438a9a
darius <darius@68-190-208-101.dhcp.gldl.ca.charter.com>
2009-07-29 23:22:52 -0700

    Start with the stupidest possible algorithm: recommend the most popular repos not already followed

---
+++ contest.py
@@ -0,0 +1,54 @@
+import heapq
+import itertools
+import sys
+
+err = sys.stderr
+
+def main():
+    run_on(open('download/data.txt'),
+           open('download/test.txt'))
+
+def run_on(datafile, testfile):
+    load(datafile)
+    print >>err, 'Loaded'
+    for u in gen_test_users(testfile):
+        print '%s:%s' % (u, ','.join(map(str, recommendations(u))))
+
+follows = {}                    # Repos a user follows
+followers = {}                  # Users who follow a repo
+
+def gen_test_users(testfile):
+    i = 0
+    for u in map(int, testfile.read().splitlines()):
+        if i % 100 == 0: print >>err, u
+        i += 1
+        yield u
+
+def load(datafile):
+    for line in datafile.read().splitlines():
+        u, r = map(int, line.split(':'))
+        follows.setdefault(u, []).append(r)
+        followers.setdefault(r, []).append(u)
+    print >>err, 'Data read'
+    compute_popularity()
+
+def recommendations(u):
+    return itertools.islice((r for r in popular_repos if r not in follows.get(u, ())),
+                            10)
+
+popular_repos = []
+popularity_table = {}
+
+def compute_popularity():
+    global popular_repos
+    popular_repos = followers.keys()
+    n_users = float(len(follows))
+    for r in popular_repos:
+        popularity_table[r] = len(followers.get(r, ())) / n_users
+    popular_repos.sort(reverse=True, key=popularity)
+
+def popularity(r):
+    return popularity_table[r]
+
+if __name__ == '__main__':
+    main()
